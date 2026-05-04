2c2dc8db233e2f52cb158dbe543322cf7bd45c32
Albaro Samuel Ruiz <91100656+samuelxd14@users.noreply.github.com>
2026-05-03 23:04:04 -0300

    Archivo duplicado

--- js/ejercicio23.js
+++
@@ -1,8 +0,0 @@
-import { actualizarTexto } from "./funciones23.js";
-
-const campoTexto = document.querySelector("#campoTexto");
-const textoReflejado = document.querySelector("#textoReflejado");
-
-campoTexto.addEventListener("input", () => {
-    actualizarTexto(campoTexto, textoReflejado);
-});
