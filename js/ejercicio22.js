c03152e4bf11838a5cfbbb8ed37aa2304c249cb6
Albaro Samuel Ruiz <91100656+samuelxd14@users.noreply.github.com>
2026-05-03 23:03:40 -0300

    Archivo duplicado

--- js/ejercicio22.js
+++
@@ -1,8 +0,0 @@
-import { reflejarTexto } from "./funciones22.js";
-
-const campoTexto = document.querySelector("#campoTexto");
-const textoReflejado = document.querySelector("#textoReflejado");
-
-campoTexto.addEventListener("input", () => {
-    reflejarTexto(campoTexto, textoReflejado);
-});
