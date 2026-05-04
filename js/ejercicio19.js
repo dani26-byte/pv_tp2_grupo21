2f83e7601993316d9d5333f6ae560c7af6c1af65
Albaro Samuel Ruiz <91100656+samuelxd14@users.noreply.github.com>
2026-05-03 23:03:14 -0300

    Archivo duplicado

--- js/ejercicio19.js
+++
@@ -1,9 +0,0 @@
-import { cambiarTexto } from "./funciones19.js";
-
-const boton = document.getElementById("btnCambiar");
-const parrafo = document.getElementById("texto");
-const texto = "Estamos en la parte 2 del trabajo practico n°2 - Ejercicio 19";
-
-boton.addEventListener("click", () =>{
-    cambiarTexto(parrafo, texto);
-});
