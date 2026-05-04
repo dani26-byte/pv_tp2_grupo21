0e44f74416b85cd0c4e7d77476f75511a47d9662
Albaro Samuel Ruiz <91100656+samuelxd14@users.noreply.github.com>
2026-05-03 22:59:59 -0300

    Archivo duplicado

--- js/funciones24.js
+++
@@ -1,4 +0,0 @@
-export const mostrarSeleccion = (seleccion, texto) => {
-    texto.textContent = `Lenguaje seleccionado: ${seleccion}`;
-    console.log(`Valor seleccionado en consola: ${seleccion}`);
-};
